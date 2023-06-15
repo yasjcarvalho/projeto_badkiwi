ccffce4f3d83261b63c871b6891205a8eab76e23
yasjcarvalho <yasmin.carvalho@igdema.ufal.br>
2023-06-15 16:49:50 -0300

    af

--- App.js
+++ App.js
@@ -12,18 +12,6 @@ const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-<<<<<<< HEAD
-    <NavigationContainer screenOptions={{
-      tabBarLabelStyle: {
-        fontSize: 16,
-        textAlign: 'center'
-      },
-      }}>
-
-      <Tab.Navigator initialRouteName="Home" screenOptions={{headerTitleAlign:"center"}}>
-        
-
-=======
     <NavigationContainer
       screenOptions={{
         tabBarLabelStyle: {
@@ -37,7 +25,6 @@ export default function App() {
         initialRouteName="Home"
         screenOptions={{ headerTitleAlign: 'center' }}
       >
->>>>>>> 0aa32ad3b77147b91656d7a696975d99ca0caf8f
         <Tab.Screen
           name="Home"
           component={Home}
@@ -45,26 +32,12 @@ export default function App() {
             tabBarLabel: 'Home',
             tabBarIcon: ({ color, size }) => (
               <MaterialCommunityIcons name="home" color={color} size={size} />
-<<<<<<< HEAD
-            ),
-            headerTitle:"Ingressos Online"
-=======
             )
->>>>>>> 0aa32ad3b77147b91656d7a696975d99ca0caf8f
           }}
         />
         <Tab.Screen
           name="Bairros"
           component={Bairros}
-<<<<<<< HEAD
-          options={{ 
-            tabBarLabel:"Bairros",
-            tabBarIcon:({color,size}) => (
-              <MaterialCommunityIcons name="home-group" color={color} size={size} />
-            ),
-            headerTitle:"Ingressos Online"
-           }}
-=======
           options={{
             tabBarLabel: 'Bairros',
             tabBarIcon: ({ color, size }) => (
@@ -75,21 +48,11 @@ export default function App() {
               />
             )
           }}
->>>>>>> 0aa32ad3b77147b91656d7a696975d99ca0caf8f
         />
 
         <Tab.Screen
           name="Profile"
           component={Profile}
-<<<<<<< HEAD
-          options={{ 
-            tabBarLabel:"Perfil",
-            tabBarIcon:({color,size}) => (
-              <MaterialCommunityIcons name="account" color={color} size={size} />
-            ),
-            headerTitle:"Ingressos Online"
-           }}
-=======
           options={{
             tabBarLabel: 'Perfil',
             tabBarIcon: ({ color, size }) => (
@@ -100,27 +63,17 @@ export default function App() {
               />
             )
           }}
->>>>>>> 0aa32ad3b77147b91656d7a696975d99ca0caf8f
         />
 
         <Tab.Screen
           name="Login"
           component={Login}
           options={{
-<<<<<<< HEAD
-            tabBarLabel:"Login",
-            tabBarIcon:({color,size}) => (
-            <MaterialCommunityIcons name="login" color={color} size={size} />
-          ),
-          headerTitle:"Ingressos Online"
-         }}
-=======
             tabBarLabel: 'Login',
             tabBarIcon: ({ color, size }) => (
               <MaterialCommunityIcons name="login" color={color} size={size} />
             )
           }}
->>>>>>> 0aa32ad3b77147b91656d7a696975d99ca0caf8f
         />
 
         <Tab.Screen
@@ -130,14 +83,8 @@ export default function App() {
             tabBarLabel: 'Cadastro Usuário',
             tabBarIcon: ({ color, size }) => (
               <MaterialCommunityIcons name="home" color={color} size={size} />
-<<<<<<< HEAD
-            ),
-            headerTitle:"Ingressos Online"
-           }}
-=======
             )
           }}
->>>>>>> 0aa32ad3b77147b91656d7a696975d99ca0caf8f
         />
       </Tab.Navigator>
     </NavigationContainer>
