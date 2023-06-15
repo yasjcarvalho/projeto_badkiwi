0aa32ad3b77147b91656d7a696975d99ca0caf8f
yasjcarvalho <yasmin.carvalho@igdema.ufal.br>
2023-06-15 16:12:52 -0300

    gloria

--- App.js
+++ App.js
@@ -12,71 +12,80 @@ const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <NavigationContainer screenOptions={{
-      tabBarLabelStyle: {
-        fontSize: 16,
-        textAlign: 'center'
-      },
-      headerTitle: 'Ingressos Online',
-      }}>
-
-      <Tab.Navigator initialRouteName="Home" screenOptions={{headerTitleAlign:"center"}}>
-        
-
+    <NavigationContainer
+      screenOptions={{
+        tabBarLabelStyle: {
+          fontSize: 16,
+          textAlign: 'center'
+        },
+        headerTitle: 'Ingressos Online'
+      }}
+    >
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerTitleAlign: 'center' }}
+      >
         <Tab.Screen
           name="Home"
           component={Home}
-          options={{ 
-            tabBarLabel:"Home",
-            tabBarIcon:({color,size}) => (
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color, size }) => (
               <MaterialCommunityIcons name="home" color={color} size={size} />
-            ),
+            )
           }}
         />
         <Tab.Screen
           name="Bairros"
           component={Bairros}
-          options={{ 
-            tabBarLabel:"Bairros",
-            tabBarIcon:({color,size}) => (
-              <MaterialCommunityIcons name="home-group" color={color} size={size} />
-            ),
-           }}
+          options={{
+            tabBarLabel: 'Bairros',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="home-group"
+                color={color}
+                size={size}
+              />
+            )
+          }}
         />
 
         <Tab.Screen
           name="Profile"
           component={Profile}
-          options={{ 
-            tabBarLabel:"Perfil",
-            tabBarIcon:({color,size}) => (
-              <MaterialCommunityIcons name="account" color={color} size={size} />
-            ),
-           }}
+          options={{
+            tabBarLabel: 'Perfil',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="account"
+                color={color}
+                size={size}
+              />
+            )
+          }}
         />
 
         <Tab.Screen
           name="Login"
           component={Login}
           options={{
-            tabBarLabel:"Login",
-            tabBarIcon:({color,size}) => (
-            <MaterialCommunityIcons name="login" color={color} size={size} />
-          ),
-         }}
+            tabBarLabel: 'Login',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="login" color={color} size={size} />
+            )
+          }}
         />
 
         <Tab.Screen
           name="Cadastrar Usuário"
           component={CadastrarUsuario}
-          options={{ 
-            tabBarLabel:"Cadastro Usuário",
-            tabBarIcon:({color,size}) => (
+          options={{
+            tabBarLabel: 'Cadastro Usuário',
+            tabBarIcon: ({ color, size }) => (
               <MaterialCommunityIcons name="home" color={color} size={size} />
-            ),
-           }}
+            )
+          }}
         />
-
       </Tab.Navigator>
     </NavigationContainer>
   )
--- components/Login.js
+++ components/Login.js
@@ -1,52 +1,51 @@
-import { StyleSheet, View, Text, Pressable } from 'react-native';
-import { Button, TextInput } from 'react-native-paper';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, View, Text, Pressable } from 'react-native'
+import { Button, TextInput } from 'react-native-paper'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 function Login() {
   return (
-    <View style = {styles.container}>
+    <View style={styles.container}>
       <SafeAreaView maxLength={40}>
-          <TextInput  style={styles.input} placeholder="E-mail" />
-        </SafeAreaView>
-        <SafeAreaView maxLength={40}>
-          <TextInput  style={styles.input} placeholder="Senha:" />
-        </SafeAreaView>
+        <TextInput style={styles.input} placeholder="E-mail" />
+      </SafeAreaView>
+      <SafeAreaView maxLength={40}>
+        <TextInput style={styles.input} placeholder="Senha:" />
+      </SafeAreaView>
 
-        <Button
-          title="Entrar"
-          color="#37567D"
-          onPress={() =>{
-            native()
-          }}
-        
-        
-        />
+      <View styles={styles.buttons1}>
+        <Text>Esqueceu a senha?</Text>
 
+        <Text>Cadastrar Usuário</Text>
+      </View>
 
-
+      <Button
+        mode="contained"
+        style={styles.buttons2}
+        onPress={() => {
+          native()
+        }}
+      >
+        Entrar
+      </Button>
     </View>
-  );
+  )
 }
 
-
-
 const styles = StyleSheet.create({
-
-  container:{
+  container: {
     flex: 1,
-    backgroundColor: "#6FB1FF",
-    justifyContent:"center",
-    alignItems:"center"
+    backgroundColor: '#6FB1FF',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 
-  input:{
+  input: {
     height: 45,
     width: 250,
     borderRadius: 8,
     backgroundColor: '#fff',
     paddingHorizontal: 16,
-    marginBottom: 16,
+    marginBottom: 16
   }
-
 })
-export default Login;
+export default Login
